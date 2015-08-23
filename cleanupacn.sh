9d5cf999be8e2be1c48b9fb04bdf86b843cda58c
Harshdesa <Harshdesa@users.noreply.github.com>
2015-08-23 17:57:08 -0400

    Create cleanupacn.sh

---
+++ cleanupacn.sh
@@ -0,0 +1,32 @@
+remoteuser=hbd140030
+remotecomputer1=net01.utdallas.edu
+
+ssh hbd140030@net01.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net02.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net03.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net04.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net05.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net06.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net07.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net08.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net09.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net10.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net11.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net12.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net13.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net14.utdallas.edu killall -u hbd140030 &
+
+ssh hbd140030@net15.utdallas.edu killall -u hbd140030 &
